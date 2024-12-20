9d1de1f757929306c687bd5c66793099e02329eb
ruanshudong <ruanshudong@qq.com>
2024-12-20 20:58:36 +0800

    update  ts/js template use getServant

--- templates/javascript/tars/app.js
+++ templates/javascript/tars/app.js
@@ -3,13 +3,14 @@ const path = require("path")
 // TODO:write .tars file in protocol directory，and use tars2node to parse it to javascript server side code
 const ${APP}  = require("./protocol/${TARSFILE_NAME}Imp").${APP} 
 
-const svr = new Tars.server() // eslint-disable-line
-const impMap = {
-  // "${APP}.${SERVER}.${OBJ}": ${APP}.someImp // TODO: specify the implement object
-}
-svr.initialize(process.env.TARS_CONFIG || path.resolve(__dirname, "./dev.config.conf"), function (server){
-  const servantName = `${server.Application}.${server.ServerName}.${OBJ}`
-  server.addServant(impMap[servantName], servantName)
-})
-svr.start()
-console.log("tars server started")
+const APP_NAME = "${APP}", SERVER_NAME = "${SERVER}", OBJ_NAME = "${OBJ}";
+let servantName = `${APP_NAME}.${SERVER_NAME}.${OBJ_NAME}`;
+let impMap = {
+    [servantName] : ${APP}.${TARSFILE_NAME}Imp
+};
+
+Tars.server.getServant(process.env.TARS_CONFIG || path.resolve(__dirname, "../dev.config.conf")).forEach(function (config){
+    console.log(config)
+    let svr = Tars.server.createServer(impMap[config.servant]);
+    svr.start(config);
+});
--- templates/typescript/tars/src/app.ts
+++ templates/typescript/tars/src/app.ts
@@ -3,13 +3,14 @@ import path from "path"
 // TODO:write .tars file in protocol directory，and use tars2node to parse it to typescript server side code
 import {${APP}} from "./protocol/${TARSFILE_NAME}Imp"
 
-const svr = new Tars.server() // eslint-disable-line
-const impMap:any = {
-  // "${APP}.${SERVER}.${OBJ}": ${APP}.someImp // TODO: specify the implement object
-}
-svr.initialize(process.env.TARS_CONFIG || path.resolve(__dirname, "../dev.config.conf"), (server)=>{
-  const servantName = `${server.Application}.${server.ServerName}.${OBJ}`
-  server.addServant(impMap[servantName], servantName)
-})
-svr.start()
-console.log("tars server started")
+const APP_NAME = "${APP}", SERVER_NAME = "${SERVER}", OBJ_NAME = "${OBJ}";
+let servantName = `${APP_NAME}.${SERVER_NAME}.${OBJ_NAME}`;
+let impMap = {
+    [servantName] : ${APP}.${TARSFILE_NAME}Imp
+};
+
+Tars.server.getServant(process.env.TARS_CONFIG || path.resolve(__dirname, "../dev.config.conf")).forEach(function (config){
+    console.log(config)
+    let svr = Tars.server.createServer(impMap[config.servant]);
+    svr.start(config);
+});
